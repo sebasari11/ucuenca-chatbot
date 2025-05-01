32a93ee0bdb63f3c25880dec0a93aca7474d434c
Sebastian Arias <sebasari11@gmail.com>
2025-05-01 17:49:22 -0500

    Refactor ask and search servie and api route

--- app/services/ask_service.py
+++ app/services/ask_service.py
@@ -1,3 +1,5 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.schemas.ask import AskRequest, AskResponse
 from app.services.search_service import perform_search
 from app.schemas.search import SearchRequest
@@ -5,24 +7,29 @@ import requests
 from app.utils.nlp import build_contextual_prompt
 
 
-def answer_question(request: AskRequest) -> AskResponse:
-    search_response = perform_search(
-        SearchRequest(query=request.query, top_k=request.top_k, backend="sentence")
-    )
-    context_chunks = [r.content for r in search_response.results]
+class AskService:
+    def __init__(self, session: AsyncSession):
+        self.session: AsyncSession = session
+        self.search_service = perform_search
 
-    context = "\n".join(context_chunks)
-
-    prompt: str = build_contextual_prompt(context, request.query)
-
-    try:
-        response = requests.post(
-            "http://localhost:11434/api/generate",
-            json={"model": request.model, "prompt": prompt, "stream": False},
-            timeout=30,
+    async def answer_question(request: AskRequest) -> AskResponse:
+        search_response = perform_search(
+            SearchRequest(query=request.query, top_k=request.top_k, backend="sentence")
         )
-        response.raise_for_status()
-        answer = response.json().get("response", "").strip()
-        return AskResponse(answer=answer, context_chunks=context_chunks)
-    except requests.RequestException as e:
-        return f"Error al comunicarse con Ollama ({request.model}): {e}"
+        context_chunks = [r.content for r in search_response.results]
+
+        context = "\n".join(context_chunks)
+
+        prompt: str = build_contextual_prompt(context, request.query)
+
+        try:
+            response = requests.post(
+                "http://localhost:11434/api/generate",
+                json={"model": request.model, "prompt": prompt, "stream": False},
+                timeout=30,
+            )
+            response.raise_for_status()
+            answer = response.json().get("response", "").strip()
+            return AskResponse(answer=answer, context_chunks=context_chunks)
+        except requests.RequestException as e:
+            return f"Error al comunicarse con Ollama ({request.model}): {e}"
--- app/services/search_service.py
+++ app/services/search_service.py
@@ -1,48 +1,48 @@
-from app.faiss_index.manager import FaissManager
-from app.utils.nlp import get_embedding
-from app.models.resource_chunk import ResourceChunk
-from app.core.database import SessionLocal
-from sqlalchemy.orm import Session
-from app.schemas.search import SearchRequest, SearchResponse, SearchResult
+# from app.faiss_index.manager import FaissManager
+# from app.utils.nlp import get_embedding
+# from app.models.resource_chunk import ResourceChunk
+# from app.core.database import SessionLocal
+# from sqlalchemy.orm import Session
+# from app.schemas.search import SearchRequest, SearchResponse, SearchResult
 
 
-def perform_search(request: SearchRequest) -> SearchResponse:
-    embedding: list[float] = get_embedding(
-        request.query,
-        backend=request.backend,
-    )
-    faiss = FaissManager()
-    chunk_ids, similarities = faiss.search(embedding, k=request.top_k)
-
-    db: Session = SessionLocal()
-    results = []
-
-    for chunk_id, sim in zip(chunk_ids, similarities):
-        chunk = db.query(ResourceChunk).filter_by(id=chunk_id).first()
-        if chunk:
-            results.append(
-                SearchResult(
-                    chunk_id=chunk.id, content=chunk.chunk_text, similarity=float(sim)
-                )
-            )
-
-    return SearchResponse(results=results)
-
-
-# def perform_smart_search(request: SearchRequest) -> SearchSmartResponse:
-#     embedding: list[float] = get_embedding(request.query)
+# def perform_search(request: SearchRequest) -> SearchResponse:
+#     embedding: list[float] = get_embedding(
+#         request.query,
+#         backend=request.backend,
+#     )
 #     faiss = FaissManager()
 #     chunk_ids, similarities = faiss.search(embedding, k=request.top_k)
-#     print(chunk_ids)
 
 #     db: Session = SessionLocal()
-#     chuncks_related = []
+#     results = []
 
-#     for chunk_id in chunk_ids:
+#     for chunk_id, sim in zip(chunk_ids, similarities):
 #         chunk = db.query(ResourceChunk).filter_by(id=chunk_id).first()
-#         chuncks_related.append(chunk.chunk_text)
-#     context: str = "\n\n".join(chuncks_related)
-#     # result = get_smart_embedding(context, request.query)
-#     answer = generate_contextual_answer_with_gemma(context, request.query)
-#     print("answer gemma!", answer)
-#     return SearchSmartResponse(answer=answer)
+#         if chunk:
+#             results.append(
+#                 SearchResult(
+#                     chunk_id=chunk.id, content=chunk.chunk_text, similarity=float(sim)
+#                 )
+#             )
+
+#     return SearchResponse(results=results)
+
+
+# # def perform_smart_search(request: SearchRequest) -> SearchSmartResponse:
+# #     embedding: list[float] = get_embedding(request.query)
+# #     faiss = FaissManager()
+# #     chunk_ids, similarities = faiss.search(embedding, k=request.top_k)
+# #     print(chunk_ids)
+
+# #     db: Session = SessionLocal()
+# #     chuncks_related = []
+
+# #     for chunk_id in chunk_ids:
+# #         chunk = db.query(ResourceChunk).filter_by(id=chunk_id).first()
+# #         chuncks_related.append(chunk.chunk_text)
+# #     context: str = "\n\n".join(chuncks_related)
+# #     # result = get_smart_embedding(context, request.query)
+# #     answer = generate_contextual_answer_with_gemma(context, request.query)
+# #     print("answer gemma!", answer)
+# #     return SearchSmartResponse(answer=answer)
