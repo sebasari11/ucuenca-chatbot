83021f713dca03d10c121056f97c8a7f1116b26f
Sebastian Arias <sebasari11@gmail.com>
2025-05-15 17:19:17 -0500

    Fixed create_access_token

--- app/core/security.py
+++ app/core/security.py
@@ -1,3 +1,5 @@
+from typing import Any
+from fastapi import HTTPException
 from passlib.context import CryptContext
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
@@ -5,8 +7,8 @@ from app.core.config import settings
 from app.core.logging import get_logger
 
 SECRET_KEY: str = settings.SECRET_KEY
-ALGORITHM = settings.ALGORITHM
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ALGORITHM: str = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 logger = get_logger(__name__)
@@ -22,7 +24,7 @@ def get_password_hash(password):
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
-    expire = datetime.now() + (
+    expire = datetime.utcnow() + (
         expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     )
     to_encode.update({"exp": expire})
@@ -30,5 +32,4 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
 
 
 def decode_token(token: str):
-    logger.debug(f"Decoding token: {token}")
     return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
